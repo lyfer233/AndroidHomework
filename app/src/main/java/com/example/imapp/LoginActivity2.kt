ae2aff4b3821c9a42d7d3c08aa948d59b5b19fd3
xiao <786181437@qq.com>
2021-05-24 11:53:42 +0800

    登录界面完成

--- app/src/main/java/com/example/imapp/LoginActivity2.kt
+++ app/src/main/java/com/example/imapp/LoginActivity2.kt
@@ -1,11 +1,31 @@
 package com.example.imapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()                    //隐藏标题栏
         setContentView(R.layout.activity_login2)
+        val buttonLogin : Button = findViewById(R.id.buttonLogin)
+        buttonLogin.setOnClickListener{
+            var key = 0
+            val userName : EditText = findViewById(R.id.editTextTextPersonName)
+            val password : EditText = findViewById(R.id.editTextTextPassword)
+            password.toString()
+            print(password)
+            if (key == 1){
+                val intent = Intent(this, mainfirst::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else{
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/imapp/MainActivity.kt
+++ app/src/main/java/com/example/imapp/MainActivity.kt
@@ -9,19 +9,13 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //val decorView = window.decorView
-        //decorView.systemUiVisibility=
-        //    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        //    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-
-
+        supportActionBar?.hide()                    //隐藏标题栏
         setContentView(R.layout.activity_main)
         val buttonIn: Button = findViewById(R.id.button1)
         buttonIn.setOnClickListener{
             val intent = Intent(this, LoginActivity2::class.java)
             startActivity(intent)
-            //finish()
+            finish()
         }
     }
 }
