0abf59345dd5eba8005f2dddb8dabe1ef4297726
xiao <786181437@qq.com>
2021-05-31 19:14:29 +0800

    数据库 3/4 目录

--- app/src/main/java/com/example/imapp/mainfirst.kt
+++ app/src/main/java/com/example/imapp/mainfirst.kt
@@ -2,10 +2,27 @@ package com.example.imapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 
 class mainfirst : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()                    //隐藏标题栏
         setContentView(R.layout.activity_mainfirst)
+        val view : ViewPager2 = findViewById(R.id.viewPager2)
+        view.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount() = 3
+
+            override fun createFragment(position: Int): Fragment {
+                return when(position) {
+                    1-> LeftFragment()
+                    0-> MiddleFragment()
+                    else-> RightFragment()
+                }
+            }
+
+        }
     }
 }
